326cc052d35821b02eadf79dfc8512063fda09ad
Milad Safi <miladsafi03@gmail.com>
2022-05-11 05:32:41 -0500

    Changes after break

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -2,8 +2,14 @@
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+		
+		
 	}
 
 }
